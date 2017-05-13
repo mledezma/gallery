d69718d847cd5489df34d9b2867b56c1bd134084
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-05-13 11:47:39 -0600

    Added more images and more functionality

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ var Gallery = function(data){
     this.url = data.url;
     this.alt = data.alt;
     this.index = 0;    
-    this.total = data.url.length;    
+    this.total = data.url.length;   
+    this.gallery = document.createElement('div');     
 }
 
 // Gallery Prototype
@@ -12,8 +13,7 @@ Gallery.prototype = {
     height: '500px',
     width: '1000px',
     imgSize: '500px',
-    gallery: document.createElement('div'),
-    container: document.getElementById('galleryContainer'),
+    container: document.createElement('div'),
     init: function(){
         // Resets the html
         this.reset();
@@ -26,18 +26,24 @@ Gallery.prototype = {
 
         // Inits the forward button
         this.forward();
+
+        // Inits the counter
+        this.renderCounter();
     },
     renderGallery: function(){
         this.gallery.style.width = this.width;
         this.gallery.style.height = this.height;
         this.gallery.style.display = 'inline';
+        // Create Image
         var img = document.createElement('img');
         img.src = this.url[this.index];
         img.alt = this.alt[this.index];
         img.style.width = this.imgSize;
         img.style.height = this.imgSize;
         this.gallery.appendChild(img);       
-        this.container.appendChild(this.gallery);    
+        this.container.appendChild(this.gallery); 
+        document.body.appendChild(this.container);
+        console.log(this.container.getElementsByTagName('div').length)
     },
     backward: function(){
         var i = document.createElement('i');
@@ -46,8 +52,8 @@ Gallery.prototype = {
         i.setAttribute('aria-hidden', 'true');
         i.style.fontSize = '40px';
         i.style.cursor = 'pointer';
+        i.style.marginRight = '30px';        
         i.addEventListener('click', function(){
-            console.log(_self);
             if(_self.index <= 0) {
                 _self.index = _self.total-1;
                 _self.alt[_self.total-1];                
@@ -67,8 +73,8 @@ Gallery.prototype = {
         i.setAttribute('aria-hidden', 'true');
         i.style.fontSize = '40px';
         i.style.cursor = 'pointer';
+        i.style.marginLeft = '30px';
         i.addEventListener('click', function(){
-            console.log(_self);
             if(_self.index >= _self.total-1) {
                 _self.index = 0;
                 _self.alt[0];
@@ -81,15 +87,26 @@ Gallery.prototype = {
         })
         this.gallery.appendChild(i);      
     },
-    reset: function(){
-        this.gallery.innerHTML = '';        
-        this.container.innerHTML = '';
+    renderCounter: function(){
+        var p = document.createElement('p');
+        p.innerHTML = this.index+1 + ' / ' + this.total;
+        p.style.marginLeft = '300px';
+        this.gallery.appendChild(p);
     },
+    reset: function(){
+        this.gallery.innerHTML = '';    
+    },
+    
     
 }
 var g1 = new Gallery({
     'url': ['img/tortugas/tortuga1.jpg','img/tortugas/tortuga2.jpg','img/tortugas/tortuga3.jpg','img/tortugas/tortuga4.jpg','img/tortugas/tortuga5.jpg'],
-    'alt': ['Imagen de Tortuga1','Imagen de Tortuga2','Imagen de Tortuga3','Imagen de Tortuga4','Imagen de Tortuga5'],
+    'alt': ['Imagen de Tortuga1','Imagen de Tortuga2','Imagen de Tortuga3','Imagen de Tortuga4','Imagen de Tortuga5']
 });
-
 g1.init();
+
+var g2 = new Gallery({
+    'url': ['img/animales/owl.jpg','img/animales/elefant.jpg','img/animales/bear.jpg','img/animales/black-leopard.jpg','img/animales/turtle.jpg',],
+    'alt': ['Imagen de un buho','Imagen de un elefante','Imagen de un oso','Imagen de un leopardo negro','Imagen de una tortuga']  
+});
+g2.init();
