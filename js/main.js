995f90b66a0837c05048cb43b24e475c3e524a45
Marco <marcoledezmacordero09@gmail.com>
2017-05-13 22:45:14 -0600

    Render
    Fixed the render, now it doent renders what I dont need

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ Gallery.prototype = {
     width: '1000px',
     imgSize: '500px',
     container: document.createElement('div'),
-    init: function(){
+    init: function() {
         // Resets the html
         this.reset();
 
@@ -30,10 +30,12 @@ Gallery.prototype = {
         // Inits the counter
         this.renderCounter();
     },
-    renderGallery: function(){
+    renderGallery: function() {
+        // Gallery Styles
         this.gallery.style.width = this.width;
         this.gallery.style.height = this.height;
-        this.gallery.style.display = 'inline';
+        this.gallery.style.display = 'inline'; 
+
         // Create Image
         var img = document.createElement('img');
         img.src = this.url[this.index];
@@ -43,9 +45,8 @@ Gallery.prototype = {
         this.gallery.appendChild(img);       
         this.container.appendChild(this.gallery); 
         document.body.appendChild(this.container);
-        console.log(this.container.getElementsByTagName('div').length)
     },
-    backward: function(){
+    backward: function() {
         var i = document.createElement('i');
         var _self = this;        
         i.classList.add('fa','fa-backward');
@@ -54,6 +55,8 @@ Gallery.prototype = {
         i.style.cursor = 'pointer';
         i.style.marginRight = '30px';        
         i.addEventListener('click', function(){
+            var img = this.parentNode.getElementsByTagName('img')[0];
+            var p = this.parentNode.getElementsByTagName('p')[0];
             if(_self.index <= 0) {
                 _self.index = _self.total-1;
                 _self.alt[_self.total-1];                
@@ -62,11 +65,14 @@ Gallery.prototype = {
                 _self.index--; 
                 _self.alt[_self.index];                                           
             };
-            _self.init();
+            img.src = _self.url[_self.index];
+            img.alt = _self.alt[_self.index];
+            p.innerHTML = _self.index+1 + ' / ' + _self.total;
+            
         })
         this.gallery.insertBefore(i, this.gallery.firstChild);      
     },
-    forward: function(){
+    forward: function() {
         var i = document.createElement('i');
         var _self = this;                
         i.classList.add('fa','fa-forward');
@@ -75,36 +81,42 @@ Gallery.prototype = {
         i.style.cursor = 'pointer';
         i.style.marginLeft = '30px';
         i.addEventListener('click', function(){
+            var img = this.parentNode.getElementsByTagName('img')[0];
+            var p = this.parentNode.getElementsByTagName('p')[0];
             if(_self.index >= _self.total-1) {
                 _self.index = 0;
                 _self.alt[0];
             }
-            else{
+            else {
                 _self.index++; 
                 _self.alt[_self.index];                          
             };
-            _self.init();
+            img.src = _self.url[_self.index];
+            img.alt = _self.alt[_self.index];
+            p.innerHTML = _self.index+1 + ' / ' + _self.total;
         })
         this.gallery.appendChild(i);      
     },
-    renderCounter: function(){
+    renderCounter: function() {
         var p = document.createElement('p');
         p.innerHTML = this.index+1 + ' / ' + this.total;
         p.style.marginLeft = '300px';
         this.gallery.appendChild(p);
     },
-    reset: function(){
+    reset: function() {
         this.gallery.innerHTML = '';    
     },
     
     
 }
+// Gallery 1
 var g1 = new Gallery({
     'url': ['img/tortugas/tortuga1.jpg','img/tortugas/tortuga2.jpg','img/tortugas/tortuga3.jpg','img/tortugas/tortuga4.jpg','img/tortugas/tortuga5.jpg'],
     'alt': ['Imagen de Tortuga1','Imagen de Tortuga2','Imagen de Tortuga3','Imagen de Tortuga4','Imagen de Tortuga5']
 });
 g1.init();
 
+// Gallery 2
 var g2 = new Gallery({
     'url': ['img/animales/owl.jpg','img/animales/elefant.jpg','img/animales/bear.jpg','img/animales/black-leopard.jpg','img/animales/turtle.jpg',],
     'alt': ['Imagen de un buho','Imagen de un elefante','Imagen de un oso','Imagen de un leopardo negro','Imagen de una tortuga']  
